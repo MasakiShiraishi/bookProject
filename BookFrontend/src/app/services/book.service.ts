7249c78ea336e6ac52ecaab28a1eec7892aa1392
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-12-31 11:36:11 +0100

    created book service to communicate with backend

---
+++ BookFrontend/src/app/services/book.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = 'http://localhost:5117/api/Books'; 
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+}
