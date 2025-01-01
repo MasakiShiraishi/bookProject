4fd2372e9060850b3c4a719755301d7da85ad240
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-01 11:33:42 +0100

    updated files for related login function

--- BookFrontend/src/app/app.routes.ts
+++ BookFrontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { AddBookComponent } from './add-book/add-book.component';
 import { EditBookComponent } from './edit-book/edit-book.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: BookListComponent },
   { path:'add-book', component: AddBookComponent },
-  { path: 'edit-book/:id', component: EditBookComponent }
+  { path: 'edit-book/:id', component: EditBookComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
