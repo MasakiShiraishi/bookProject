5319620e8806ace0dd9ceb703e8f00a0c6703a62
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-02 07:44:14 +0100

    fixed error for related components

--- BookFrontend/src/app/app.routes.ts
+++ BookFrontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookListComponent } from './component/book-list/book-list.component';
-import { AddBookComponent } from './component/add-book/add-book.component';
-import { EditBookComponent } from './component/edit-book/edit-book.component';
-import { LoginComponent } from './component/login/login.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
